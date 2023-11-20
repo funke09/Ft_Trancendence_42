c4e77013a6f9c82a4c68d90af573b87da48266ab
khimya <benbraitit@gmail.com>
2023-11-20 01:48:40 +0100

    navbarHeader

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    API_URL: process.env.API_URL || "https://api.example.com",
+  },
+};
+
 
 module.exports = nextConfig
