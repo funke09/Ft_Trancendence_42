97bb0d95f20845c32b11d1a72dec300583482dd4
haytham10 <cnarabi@gmail.com>
2023-11-23 22:30:02 +0100

    features page DONE

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -6,17 +6,17 @@ const Navbar: React.FC = () => {
 	  <div className="header p-4 md:p-8 flex">
 		<div className="flex flex-col justify-center mx-auto my-20 m-12">
 		  <div className="text-container">
-			<h1 className="text-white text-2xl font-bold Manrope tracking-wider mb-4 text-center">
+			<h1 className="text-white text-4xl font-bold Manrope tracking-wider mb-4 text-center">
 			  Classic Pong
 			<hr className="border-1 border-white opacity-50 m-4" />
 			</h1>
-			<div className="text-align text-white text-2xl font-medium Manrope tracking-wider mb-4">
+			<div className="text-align text-white text-3xl font-semibold Manrope tracking-wider mb-4">
 			  Enjoy the timeless thrill of <br />
 			  bouncing balls and <br />
 			  competitive fun. <br />
 			</div>
 		  </div>
-		  <button className="pink-button m-12">Get Started</button>
+		  <button className="pink-button mt-5 ">Get Started</button>
 		</div>
   
 		<div className="m-4 flex flex-grow justify-end">
--- frontend/src/pages/features.tsx
+++ frontend/src/pages/features.tsx
@@ -1,6 +1,7 @@
 import next from "next";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 import Navbar from "@/components/Layout/NavBar";
 
 const Features: React.FC = () => {
@@ -15,18 +16,101 @@ const Features: React.FC = () => {
 					<div className="features-container flex justify-between m-8">
           {/* Left Bars */}
           <div className="flex flex-col items-center">
-            <div className="features-slides">
-				<div></div>
+		  <div className="features-slides">
+			<div className="flex items-center ">
+				<Image
+				src="/images/online.png"
+				alt="online"
+				width={70}
+				height={70}
+				className="pr-3"
+				></Image>
+				<div>
+				<h3 className="text-white text-xl font-semibold">Unleash the thrill of real-time battles</h3>
+				<h3 className="text-white text-xl font-semibold">where every move counts</h3>
+				</div>
 			</div>
-            <div className="features-slides" />
-            <div className="features-slides" />
+			</div>
+            <div className="features-slides">
+			<div className="flex items-center ">
+				<Image
+				src="/images/friends.png"
+				alt="firends"
+				width={70}
+				height={70}
+				className="pr-3"
+				></Image>
+				<div>
+				<h3 className="text-white text-xl font-semibold">Forge bonds and strategize with your</h3>
+				<h3 className="text-white text-xl font-semibold">crew in dedicated chat channels</h3>
+				</div>
+			</div>
+			</div>
+			
+            <div className="features-slides">
+			<div className="flex items-center ">
+				<Image
+				src="/images/message.png"
+				alt="message"
+				width={70}
+				height={70}
+				className="pr-3"
+				></Image>
+				<div>
+				<h3 className="text-white text-xl font-semibold">Connect, banter, and cheer on your</h3>
+				<h3 className="text-white text-xl font-semibold">teammates with our chat system.</h3>
+				</div>
+			</div>
+			</div>
           </div>
 
           {/* Right Bars */}
           <div className="flex flex-col items-center">
-            <div className="features-slides" />
-            <div className="features-slides" />
-            <div className="features-slides" />
+            <div className="features-slides">
+			<div className="flex items-center ">
+				<Image
+				src="/images/leaderboard.png"
+				alt="leaderboard"
+				width={70}
+				height={70}
+				className="pr-3"
+				></Image>
+				<div>
+				<h3 className="text-white text-xl font-semibold">Rise to the top and let the world know</h3>
+				<h3 className="text-white text-xl font-semibold">who dominates the arena!</h3>
+				</div>
+			</div>
+			</div>
+            <div className="features-slides">
+			<div className="flex items-center ">
+				<Image
+				src="/images/arcade.png"
+				alt="arcade"
+				width={70}
+				height={70}
+				className="pr-3"
+				></Image>
+				<div>
+				<h3 className="text-white text-xl font-semibold">Dive into a world where every battle</h3>
+				<h3 className="text-white text-xl font-semibold">is an adventure that keeps you hooked</h3>
+				</div>
+			</div>
+			</div>
+            <div className="features-slides">
+			<div className="flex items-center ">
+				<Image
+				src="/images/medal.png"
+				alt="medal"
+				width={70}
+				height={70}
+				className="pr-3"
+				></Image>
+				<div>
+				<h3 className="text-white text-xl font-semibold">Celebrate every victory, big or small! </h3>
+				<h3 className="text-white text-xl font-semibold">Achievements for each milestone</h3>
+				</div>
+			</div>
+			</div>
           </div>
         </div>
 				</div>
