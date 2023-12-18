016ca3199d1fd3bc620ff14287a5fbb70d37fa52
haytham10 <cnarabi@gmail.com>
2023-12-18 14:16:43 +0100

    fix merge conflict

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -21,7 +21,7 @@ const Header: React.FC = () => {
 			</div>
 		  </div>
 		  {isAuth ? <button className=" nav-button hover bg-[#F53FA1] bg-opacity-[75%] rounded-full m-4 w-fit p-4 text-white text-[2rem] font-bold Manrope text-opacity-[100%] hover:bg-opacity-100">
-			GET STARTED</button> : <button onClick={() => router.push('/login')} className="bg-[#F53FA1] bg-opacity-[75%] rounded-full m-4 w-fit p-4 text-white text-[2rem] font-bold Manrope text-opacity-[100%] hover:bg-opacity-100">
+			GET STARTED</button> : <button onClick={handleOpen} className="bg-[#F53FA1] bg-opacity-[75%] rounded-full m-4 w-fit p-4 text-white text-[2rem] font-bold Manrope text-opacity-[100%] hover:bg-opacity-100">
 			GET STARTED</button>
 			}
 		</div>
--- frontend/src/pages/team.tsx
+++ frontend/src/pages/team.tsx
@@ -7,8 +7,9 @@ const Team: React.FC = () => {
     const teamMembers = [
         { id: 1, name: "Funke09", githubUsername: "funke09", role: "Developer" },
         { id: 2, name: "haytham10", githubUsername: "haytham10", role: "Developer" },
-        { id: 3, name: "0xPacman", githubUsername: "0xPacman", role: "Developer" },
-        { id: 4, name: "YOPll", githubUsername: "YOPll", role: "Developer" },
+        { id: 3, name: "Oussamazz", githubUsername: "Oussamazz", role: "Developer" },
+        { id: 4, name: "0xPacman", githubUsername: "0xPacman", role: "Developer" },
+        { id: 5, name: "YOPll", githubUsername: "YOPll", role: "Developer" },
     ];
 
 	const [loading, setLoading] = useState(true);
