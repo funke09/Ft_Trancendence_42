aec08e7cd5e2e7ee18dd00bb5b25f785140aa20e
haytham10 <cnarabi@gmail.com>
2023-12-19 07:28:17 +0100

    added rank border, achievements list

--- frontend/src/components/User/Dashboard.tsx
+++ frontend/src/components/User/Dashboard.tsx
@@ -28,15 +28,49 @@ interface GameData {
 	games: Game[];
 }
 
-function getAvatarBorder(rank: string): string | undefined {
+function getAvatarBorder(rank: string | undefined): string | undefined {
 	if (rank === "Bronze")
 		return "flex justify-center border border-[#CD7F32] shadow-xl shadow-[#CD7F32]/40 ring-4 ring-[#CD7F32]"
-	/// ...
+	else if (rank === "Silver")
+		return "flex justify-center border border-[#C0C0C0] shadow-xl shadow-[#C0C0C0]/40 ring-4 ring-[#C0C0C0]"
+	else if (rank === "Gold")
+		return "flex justify-center border border-[#CD7F32] shadow-xl shadow-[#CD7F32]/40 ring-4 ring-[#CD7F32]"
+	else if (rank === "Platinum")
+		return "flex justify-center border border-[#40E0D0] shadow-xl shadow-[#40E0D0]/40 ring-4 ring-[#40E0D0]"
+	else if (rank === "Diamond")
+		return "flex justify-center border border-[#b9f2ff] shadow-xl shadow-[#b9f2ff]/40 ring-4 ring-[#b9f2ff]"
+	else
+		return "flex justify-center border border-[#D9D9D9] shadow-xl shadow-[#D9D9D9]/40 ring-4 ring-[#D9D9D9]"
+
+}
+
+function Achiev({ stats }: any) {
+	return (
+		<Card className="container rounded-[15px] bg-[#472C45] max-h-[320px] mt-2 overflow-y-auto">
+			<List>
+				{stats?.stats?.achievements?.map(
+					(achiev: {id: number; name: string; desc: string; icon: string; userId: number}) => {
+						console.log(api.getUri() + achiev.icon);
+						return (
+							<Tooltip className="bg-gray-700 bg-opacity-80" content={achiev.desc} placement="top" animate={{mount: { scale: 1, x: 0 }, unmount: { scale: 0, x: -25 },}}>
+								<ListItem key={achiev.id} className="p-1 text-white hover:text-[#472C45]">
+									<ListItemPrefix>
+										<Image src={achiev.icon} width={50} height={50} alt={achiev.name} className="rounded-full"/>
+									</ListItemPrefix>
+									<Typography variant="h6">{achiev.name}</Typography>
+								</ListItem>
+							</Tooltip>
+						)
+					}
+				)}
+			</List>
+		</Card>
+	)
 }
 
 function Dashboard({ id }: {id: string}) {
 	const user = store.getState().profile.user;
-	const [currUser, setCurrUser] = useState<any>(null);
+	const [profile, setProfile] = useState<any>(null);
 	const [stats, setStats] = useState<any>(null);
 	const [error, setError] = useState(false);
 
@@ -44,7 +78,7 @@ function Dashboard({ id }: {id: string}) {
         api.get("/user/id/" + id)
             .then((res: any) => {
                 if (res.status == 200) {
-                    setCurrUser(res.data);
+                    setProfile(res.data);
                 }
             })
             .catch((err: any) => {
@@ -57,47 +91,38 @@ function Dashboard({ id }: {id: string}) {
             .catch((err: AxiosError<{ message: string }>) => {});
     }, []);
 
-	if (!currUser) return <></>
-
+	if (!profile) return <></>
 
 	return (
 	  <div>
 		  <main className="min-h-[720px] max-w-[1200px] rounded-[15px] flex m-auto bg-[#472C45] opacity-80">
-		  	<section className="w-1/4 bg-[#643461] p-3 flex-col justify-center rounded-[15px]">
+		  	<section className="w-full smool:w-1/4 bg-[#643461] p-3 flex-col justify-center rounded-[15px]">
+				{profile && profile.username == user.username ?
 				<IconButton size="sm" className="m-2 rounded-full shadow-md bg-[#351633] hover:scale-110 border-none">
 					<i className="fa-solid fa-gear fa-xl" style={{ color: "#E1E1E1" }} />
 				</IconButton>
+				:
+				<IconButton size="sm" className="m-2 rounded-full shadow-md bg-[#351633] hover:scale-110 border-none">
+					<i className="fa-sharp fa-solid fa-user-plus pl-0.5" style={{ color: "#E1E1E1" }} />
+				</IconButton>
+				}
 			  	<div className="flex-col flex items-center justify-start gap-3 pb-4">
-					<Avatar src={user.avatar} variant="rounded" size="xxl" className={getAvatarBorder(stats?.stats.rank)}/>
-					<Typography variant="h3" className="flex justify-center text-white font-bold">{user.username}</Typography>
-					<Typography className="flex justify-center text-[18px] text-gray-400">{user.email}</Typography>
+				  <Tooltip className="bg-[#472C45] bg-opacity-70" content={stats?.stats.rank} placement="top" offset={10} animate={{mount: { scale: 1, y: 0 }, unmount: { scale: 0, y: 25 },}}>
+						<Avatar src={profile.avatar} variant="rounded" size="xxl" className={getAvatarBorder(stats?.stats.rank)}/>
+				  </Tooltip>
+					<Typography variant="h3" className="flex justify-center text-white font-bold">{profile.username}</Typography>
+					<Typography className="flex justify-center text-[18px] text-gray-400">{profile.email}</Typography>
 				</div>
 				<hr className="mx-5 rounded-full opacity-75 border-[1px] mb-3"/>
 				<Typography className="flex justify-center text-[24px] text-white font-normal">Achievements</Typography>
-				<Card className="container rounded-[15px] bg-[#472C45] min-h-[300px] mt-2">
-					<List>
-						<Tooltip content="Achievenet Description" placement="right" animate={{mount: { scale: 1, x: 0 }, unmount: { scale: 0, x: -25 },}}>
-							<ListItem>
-								<ListItemPrefix>
-									<Image src={'/'} width={30} height={30} alt="icon of achievment"/>
-								</ListItemPrefix>
-								<Typography variant="h6" className="text-white ">Name of Achievement</Typography>
-							</ListItem>
-						</Tooltip>
-						<Tooltip content="Achievemet Description" placement="right" animate={{mount: { scale: 1, x: 0 }, unmount: { scale: 0, x: -25 },}}>
-							<ListItem disabled={true}>
-								<ListItemPrefix>
-									<Image src={'/'} width={30} height={30} alt="icon of achievment"/>
-								</ListItemPrefix>
-								<Typography variant="h6" className="text-white ">Name of Achievement</Typography>
-							</ListItem>
-						</Tooltip>
-					</List>
-				</Card>
+				{stats && stats?.stats && <Achiev stats={stats}/>}
 		  	</section>
+			<section>
+					
+			</section>
 		  </main>
-			
 	  </div>
 	);
-  }
-	export default Dashboard;
+}
+
+export default Dashboard;
