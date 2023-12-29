8fe759ef0512a096802c537fbd71204cd9fa99fa
haytham10 <cnarabi@gmail.com>
2023-12-29 06:59:01 +0100

    User verification for chat

--- frontend/src/pages/chat.tsx
+++ frontend/src/pages/chat.tsx
@@ -6,6 +6,25 @@ import store, { setProfile } from "@/redux/store";
 import { ChatRoom } from "@/components/Chat/ChatRoom";
 
 const Chat: React.FC = () => {
+	const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        api.get("/user/profile")
+            .then((res: any) => {
+                if (res.status == 200) {
+                    store.dispatch(setProfile(res.data));
+                    setLoading(false);
+                } else {
+                    window.location.href = "/";
+                }
+            })
+            .catch((err: any) => {
+                window.location.href = "/login";
+            });
+    }, []);
+
+	if (loading) {
+		return(<Loading/>);
+	} 
 
     return (
         <div>
