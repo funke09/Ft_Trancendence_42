919d5c034510919d1781229db589fa48a444b4f1
haytham10 <cnarabi@gmail.com>
2023-12-30 11:27:30 +0100

    Fix: - Password validation

--- backend/src/user/channel.service.ts
+++ backend/src/user/channel.service.ts
@@ -11,10 +11,14 @@ export class ChannelService {
 		const user = await this.prisma.user.findUnique({where: {id: id}});
 		if (!user) throw new NotFoundException("User not found");
 
-		if (channel.password && channel.password.length < 8) {
-			throw new BadRequestException('Channel Password must be at least 8 characters long');
+		if (channel.type === 'protected')
+		{
+			if (!channel.password) throw new BadRequestException("No Password provided");
+			if (channel.password && channel.password.length < 8)
+				throw new BadRequestException('Channel Password must be at least 8 characters long');
+			channel.password = await argon.hash(channel.password);
+
 		}
-		const hash = channel.type === 'protected' ? await argon.hash(channel.password) : null;
 
 		if (['private', 'protected', 'public'].includes(channel.type) == false) {
 			throw new BadRequestException("Invalid channel type");
@@ -31,7 +35,7 @@ export class ChannelService {
 				admins: {connect: {id: user.id}},
 				name: channel.name,
 				type: channel.type,
-				password: hash,
+				password: channel.password,
 				ownerId: user.id,
 				adminsIds: { set: [user.id] }
 			},
