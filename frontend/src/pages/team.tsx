ff7fc0adb79563b6538ccd0579e66702df0d0a1b
0xPacman <ahmed4star@gmail.com>
2024-01-06 18:29:20 +0100

    updating the team page and shi l3ibat

--- frontend/src/pages/team.tsx
+++ frontend/src/pages/team.tsx
@@ -5,11 +5,11 @@ import { Nav } from "@/components/Layout/NavBar";
 
 const Team: React.FC = () => {
     const teamMembers = [
-        { id: 1, name: "Funke09", githubUsername: "funke09", role: "Developer" },
-        { id: 2, name: "haytham10", githubUsername: "haytham10", role: "Developer" },
-        { id: 3, name: "Oussamazz", githubUsername: "Oussamazz", role: "Developer" },
-        { id: 4, name: "0xPacman", githubUsername: "0xPacman", role: "Developer" },
-        { id: 5, name: "YOPll", githubUsername: "YOPll", role: "Developer" },
+        { id: 1, name: "Funke09", githubUsername: "funke09", role: "Full-Stack Developer" },
+        { id: 2, name: "haytham10", githubUsername: "haytham10", role: "Full-Stack Engineer" },
+        { id: 3, name: "Oussamazz", githubUsername: "Oussamazz", role: "DevOps enthusiast" },
+        { id: 4, name: "0xPacman", githubUsername: "0xPacman", role: "Infra & Cloud Engineer" },
+        { id: 5, name: "YOPll", githubUsername: "YOPll", role: "Infra & System Specialist" },
     ];
 
 	const [loading, setLoading] = useState(true);
@@ -32,28 +32,45 @@ const Team: React.FC = () => {
         return <h1>Loading...</h1>;
     }
 
+    const images = [
+        "https://avatars.githubusercontent.com/u/92769858?v=4",
+        "https://avatars.githubusercontent.com/u/31369367?v=4",
+        "https://avatars.githubusercontent.com/u/40428234?v=4",
+        "https://avatars.githubusercontent.com/u/22503811?v=4",
+        "https://avatars.githubusercontent.com/u/49567393?v=4",
+    ];
+
+
     return (
-        <div className=" text-white">
-			<Nav/>
+        <div className="text-white">
+            <Nav/>
             <div className="text-center my-8">
-                <h1 className="text-3xl">Our Team</h1>
-                <ul className="mt-4">
+                <h1 className="text-3xl">Our Team</h1>     
+                <div className="flex flex-wrap justify-center mt-4">
                     {teamMembers.map((member) => (
-                        <li key={member.id} className="text-lg my-2">
-                            <strong>
+                        <div key={member.id} className="card m-4">
+                            <div className="card-image">
+                                { <img src={images[member.id - 1]} alt={member.name} /> }
+                            </div>
+                            <div className="card-content">
                                 <a
                                     href={`https://github.com/${member.githubUsername}`}
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    className="underline"
+                                    className="github-link"
                                 >
-                                    {member.name}
+                                    
+                                    <i className="fab fa-github"></i>
+                                    <br />
+                                    <span className="text-pink-600">{member.name}</span>
+                                    <br />
+                                    
+                                    <span>{member.role}</span>
                                 </a>
-                            </strong>{" "}
-                            - {member.role}
-                        </li>
+                            </div>
+                        </div>
                     ))}
-                </ul>
+                </div>
             </div>
         </div>
     );
